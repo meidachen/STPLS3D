8926de63089c2b989829c3d3271716e2245f645f
Meida Chen <meidache@usc.edu>
2021-09-27 15:49:07 -0700

    Initial commit

---
+++ RandLA-Net/helper_tool.py
@@ -0,0 +1,235 @@
+import open3d as open3d
+from os.path import join
+import numpy as np
+import colorsys, random, os, sys
+import pandas as pd
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'utils'))
+
+import cpp_wrappers.cpp_subsampling.grid_subsampling as cpp_subsampling
+import nearest_neighbors.lib.python.nearest_neighbors as nearest_neighbors
+
+
+class ConfigSTPLS3D:
+    k_n = 16  # KNN
+    num_layers = 5  # Number of layers
+    num_points = 40960  # Number of input points
+    num_classes = 6  # Number of valid classes
+    sub_grid_size = 0.3  # preprocess_parameter
+
+    batch_size = 3  # batch_size during training
+    val_batch_size = 20  # batch_size during validation and test
+    train_steps = 500  # Number of steps per epochs
+    val_steps = 20  # Number of validation steps per epoch
+
+    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256, 512]  # feature dimension
+
+    noise_init = 3.5  # noise initial parameter
+    max_epoch = 100  # maximum epoch during training
+    learning_rate = 1e-2  # initial learning rate
+    lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+
+
+class DataProcessing:
+
+
+    @staticmethod
+    def get_file_list(dataset_path, test_scan_num):
+        seq_list = np.sort(os.listdir(dataset_path))
+
+        train_file_list = []
+        test_file_list = []
+        val_file_list = []
+        for seq_id in seq_list:
+            seq_path = join(dataset_path, seq_id)
+            pc_path = join(seq_path, 'velodyne')
+            if seq_id == '08':
+                val_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+                if seq_id == test_scan_num:
+                    test_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+            elif int(seq_id) >= 11 and seq_id == test_scan_num:
+                test_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+            elif seq_id in ['00', '01', '02', '03', '04', '05', '06', '07', '09', '10']:
+                train_file_list.append([join(pc_path, f) for f in np.sort(os.listdir(pc_path))])
+
+        train_file_list = np.concatenate(train_file_list, axis=0)
+        val_file_list = np.concatenate(val_file_list, axis=0)
+        test_file_list = np.concatenate(test_file_list, axis=0)
+        return train_file_list, val_file_list, test_file_list
+
+    @staticmethod
+    def knn_search(support_pts, query_pts, k):
+        """
+        :param support_pts: points you have, B*N1*3
+        :param query_pts: points you want to know the neighbour index, B*N2*3
+        :param k: Number of neighbours in knn search
+        :return: neighbor_idx: neighboring points indexes, B*N2*k
+        """
+
+        neighbor_idx = nearest_neighbors.knn_batch(support_pts, query_pts, k, omp=True)
+        return neighbor_idx.astype(np.int32)
+
+    @staticmethod
+    def data_aug(xyz, color, labels, idx, num_out):
+        num_in = len(xyz)
+        dup = np.random.choice(num_in, num_out - num_in)
+        xyz_dup = xyz[dup, ...]
+        xyz_aug = np.concatenate([xyz, xyz_dup], 0)
+        color_dup = color[dup, ...]
+        color_aug = np.concatenate([color, color_dup], 0)
+        idx_dup = list(range(num_in)) + list(dup)
+        idx_aug = idx[idx_dup]
+        label_aug = labels[idx_dup]
+        return xyz_aug, color_aug, idx_aug, label_aug
+
+    @staticmethod
+    def shuffle_idx(x):
+        # random shuffle the index
+        idx = np.arange(len(x))
+        np.random.shuffle(idx)
+        return x[idx]
+
+    @staticmethod
+    def shuffle_list(data_list):
+        indices = np.arange(np.shape(data_list)[0])
+        np.random.shuffle(indices)
+        data_list = data_list[indices]
+        return data_list
+
+    @staticmethod
+    def grid_sub_sampling(points, features=None, labels=None, grid_size=0.1, verbose=0):
+        """
+        CPP wrapper for a grid sub_sampling (method = barycenter for points and features
+        :param points: (N, 3) matrix of input points
+        :param features: optional (N, d) matrix of features (floating number)
+        :param labels: optional (N,) matrix of integer labels
+        :param grid_size: parameter defining the size of grid voxels
+        :param verbose: 1 to display
+        :return: sub_sampled points, with features and/or labels depending of the input
+        """
+
+        if (features is None) and (labels is None):
+            return cpp_subsampling.compute(points, sampleDl=grid_size, verbose=verbose)
+        elif labels is None:
+            return cpp_subsampling.compute(points, features=features, sampleDl=grid_size, verbose=verbose)
+        elif features is None:
+            return cpp_subsampling.compute(points, classes=labels, sampleDl=grid_size, verbose=verbose)
+        else:
+            return cpp_subsampling.compute(points, features=features, classes=labels, sampleDl=grid_size,
+                                           verbose=verbose)
+
+    @staticmethod
+    def IoU_from_confusions(confusions):
+        """
+        Computes IoU from confusion matrices.
+        :param confusions: ([..., n_c, n_c] np.int32). Can be any dimension, the confusion matrices should be described by
+        the last axes. n_c = number of classes
+        :return: ([..., n_c] np.float32) IoU score
+        """
+
+        # Compute TP, FP, FN. This assume that the second to last axis counts the truths (like the first axis of a
+        # confusion matrix), and that the last axis counts the predictions (like the second axis of a confusion matrix)
+        TP = np.diagonal(confusions, axis1=-2, axis2=-1)
+        TP_plus_FN = np.sum(confusions, axis=-1)
+        TP_plus_FP = np.sum(confusions, axis=-2)
+
+        # Compute IoU
+        IoU = TP / (TP_plus_FP + TP_plus_FN - TP + 1e-6)
+
+        # Compute mIoU with only the actual classes
+        mask = TP_plus_FN < 1e-3
+        counts = np.sum(1 - mask, axis=-1, keepdims=True)
+        mIoU = np.sum(IoU, axis=-1, keepdims=True) / (counts + 1e-6)
+
+        # If class is absent, place mIoU in place of 0 IoU to get the actual mean later
+        IoU += mask * mIoU
+        return IoU
+
+    @staticmethod
+    def get_class_weights(dataset_name):
+        # pre-calculate the number of points in each category
+        num_per_class = []
+        if dataset_name is 'STPLS3D':
+            num_per_class = np.array([5315782, 6445416, 3669377, 170255, 31557, 89423], dtype=np.int32)
+        weight = num_per_class / float(sum(num_per_class))
+        ce_label_weight = 1 / (weight + 0.02)
+        return np.expand_dims(ce_label_weight, axis=0)
+
+
+class Plot:
+    @staticmethod
+    def random_colors(N, bright=True, seed=0):
+        brightness = 1.0 if bright else 0.7
+        hsv = [(0.15 + i / float(N), 1, brightness) for i in range(N)]
+        colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+        random.seed(seed)
+        random.shuffle(colors)
+        return colors
+
+    @staticmethod
+    def draw_pc(pc_xyzrgb):
+        pc = open3d.PointCloud()
+        pc.points = open3d.Vector3dVector(pc_xyzrgb[:, 0:3])
+        if pc_xyzrgb.shape[1] == 3:
+            open3d.draw_geometries([pc])
+            return 0
+        if np.max(pc_xyzrgb[:, 3:6]) > 20:  ## 0-255
+            pc.colors = open3d.Vector3dVector(pc_xyzrgb[:, 3:6] / 255.)
+        else:
+            pc.colors = open3d.Vector3dVector(pc_xyzrgb[:, 3:6])
+        open3d.draw_geometries([pc])
+        return 0
+
+    @staticmethod
+    def draw_pc_sem_ins(pc_xyz, pc_sem_ins, plot_colors=None):
+        """
+        pc_xyz: 3D coordinates of point clouds
+        pc_sem_ins: semantic or instance labels
+        plot_colors: custom color list
+        """
+        if plot_colors is not None:
+            ins_colors = plot_colors
+        else:
+            ins_colors = Plot.random_colors(len(np.unique(pc_sem_ins)) + 1, seed=2)
+
+        ##############################
+        sem_ins_labels = np.unique(pc_sem_ins)
+        sem_ins_bbox = []
+        Y_colors = np.zeros((pc_sem_ins.shape[0], 3))
+        for id, semins in enumerate(sem_ins_labels):
+            valid_ind = np.argwhere(pc_sem_ins == semins)[:, 0]
+            if semins <= -1:
+                tp = [0, 0, 0]
+            else:
+                if plot_colors is not None:
+                    tp = ins_colors[semins]
+                else:
+                    tp = ins_colors[id]
+
+            Y_colors[valid_ind] = tp
+
+            ### bbox
+            valid_xyz = pc_xyz[valid_ind]
+
+            xmin = np.min(valid_xyz[:, 0]);
+            xmax = np.max(valid_xyz[:, 0])
+            ymin = np.min(valid_xyz[:, 1]);
+            ymax = np.max(valid_xyz[:, 1])
+            zmin = np.min(valid_xyz[:, 2]);
+            zmax = np.max(valid_xyz[:, 2])
+            sem_ins_bbox.append(
+                [[xmin, ymin, zmin], [xmax, ymax, zmax], [min(tp[0], 1.), min(tp[1], 1.), min(tp[2], 1.)]])
+
+        Y_semins = np.concatenate([pc_xyz[:, 0:3], Y_colors], axis=-1)
+        Plot.draw_pc(Y_semins)
+        return Y_semins
---
+++ RandLA-Net/utils/data_prepare_STPLS3D.py
@@ -0,0 +1,99 @@
+from sklearn.neighbors import KDTree
+import numpy as np
+import pandas as pd
+import os, glob, pickle
+from helper_ply import write_ply
+from helper_tool import DataProcessing as DP
+
+
+
+def createFolder(folderPath):
+    if not os.path.isdir(folderPath):
+        os.makedirs(folderPath)
+
+def changeSemLabels(cloud):
+
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >= 2) &  (cloud[:, 6:7] <= 4), 2, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >= 5) &  (cloud[:, 6:7] <= 6), 3, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] == 8), 3, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >= 11) &  (cloud[:, 6:7] <= 12), 4, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] == 14), 5, cloud[:, 6:7])
+
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >= 7) &  (cloud[:, 6:7] <= 10), 1, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] == 13), 1, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >= 15) &  (cloud[:, 6:7] <= 16), 0, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] == 17), 1, cloud[:, 6:7])
+    cloud[:, 6:7] = np.where((cloud[:, 6:7] >17), 0, cloud[:, 6:7])
+
+
+    return cloud
+
+def convert_pc2ply(dataOriginal_fpath, save_path):
+
+
+    cloud = pd.read_csv(dataOriginal_fpath, delimiter=',', header=[0]).values
+    limitMin = np.amin(cloud[:, 0:3], axis=0)
+    cloud[:, 0:3] -= limitMin
+    cloud = changeSemLabels(cloud)
+    values, counts = np.unique(cloud[:, 6], return_counts=True)
+    print (values)
+    print (counts)
+    for i,value in enumerate(values):
+        labelCount[int(value)] += counts[i]
+
+    xyz = cloud[:, :3].astype(np.float32)
+    colors = cloud[:, 3:6].astype(np.uint8)
+    labels = cloud[:, 6].astype(np.uint8)
+    write_ply(save_path, (xyz, colors, labels), ['x', 'y', 'z', 'red', 'green', 'blue', 'class'])
+
+    # save sub_cloud and KDTree file
+    sub_xyz, sub_colors, sub_labels = DP.grid_sub_sampling(xyz, colors, labels, sub_grid_size)
+    sub_colors = sub_colors / 255.0
+    sub_ply_file = os.path.join(sub_pc_dir, os.path.basename(save_path))
+
+    write_ply(sub_ply_file, [sub_xyz, sub_colors, sub_labels], ['x', 'y', 'z', 'red', 'green', 'blue', 'class'])
+
+    search_tree = KDTree(sub_xyz)
+
+    kd_tree_file = os.path.join(sub_pc_dir, os.path.basename(save_path).replace('.ply','_KDTree.pkl'))
+
+    with open(kd_tree_file, 'wb') as f:
+        pickle.dump(search_tree, f)
+
+    proj_idx = np.squeeze(search_tree.query(xyz, return_distance=False))
+    proj_idx = proj_idx.astype(np.int32)
+
+    proj_save =  os.path.join(sub_pc_dir, os.path.basename(save_path).replace('.ply','_proj.pkl'))
+
+    with open(proj_save, 'wb') as f:
+        pickle.dump([proj_idx, labels], f)
+
+
+if __name__ == '__main__':
+    sub_grid_size = 0.3
+    dataOriginal_dir = r'E:\syntheticVSreal_experiements\STPLS3D\RealWorldData'
+    dataTraining_dir = r'E:\syntheticVSreal_experiements\STPLS3D_prepared\RealWorldData'
+    createFolder(dataTraining_dir)
+    original_ply_dir = os.path.join(dataTraining_dir,'original_ply')
+    createFolder(original_ply_dir)
+    sub_pc_dir = os.path.join(dataTraining_dir, 'input_{:.3f}'.format(sub_grid_size))
+    createFolder(sub_pc_dir)
+    labelCount = [0,0,0,0,0,0]
+    dataOriginal_flist = glob.glob(dataOriginal_dir + r'\*.txt')
+    for dataOriginal_fpath in dataOriginal_flist:
+        print (dataOriginal_fpath)
+        save_path = os.path.join(original_ply_dir,os.path.basename(dataOriginal_fpath).replace('.txt','.ply'))
+        convert_pc2ply(dataOriginal_fpath, save_path)
+    print (labelCount)
+
+    # labelCount = [0,0,0,0,0,0]
+    # dataOriginal_flist = glob.glob(dataOriginal_dir + r'\*.txt')
+    # for dataOriginal_fpath in dataOriginal_flist:
+    #     cloud = pd.read_csv(dataOriginal_fpath, delimiter=',', header=[0]).values
+    #     cloud = changeSemLabels(cloud)
+    #     values, counts = np.unique(cloud[:, 6], return_counts=True)
+    #     print (values)
+    #     print (counts)
+    #     for i,value in enumerate(values):
+    #         labelCount[int(value)] += counts[i]
+    # print (labelCount)
