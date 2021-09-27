c300d79db5eeaf5680531e924dea8eb41c281a1f
Meida Chen <meidache@usc.edu>
2021-09-27 16:49:44 -0700

    Added SCF net

---
+++ SCF-Net/STPLS3D_main.py
@@ -0,0 +1,311 @@
+### Set up which GPU to use
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
+from SCFNet import Network
+from STPLS3D_test import ModelTester
+from helper_ply import read_ply
+from helper_dp import DataProcessing as DP
+import numpy as np
+import time, pickle, argparse, glob, os
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+class cfg:
+    k_n = 16  # KNN
+    num_layers = 5  # Number of layers
+    num_points = 40960  # Number of input points
+    num_classes = 6  # Number of valid classes
+    sub_grid_size = 0.3  # preprocess_parameter
+
+    batch_size = 3  # batch_size during training
+    val_batch_size = 20  # batch_size during validation and test
+    train_steps = 500  # Number of steps per epochs
+    val_steps = 20  # Number of validation steps per epoch
+
+    sub_sampling_ratio = [4, 4, 4, 4, 2]  # sampling ratio of random sampling at each layer
+    d_out = [16, 64, 128, 256, 512]  # feature dimension
+
+    noise_init = 3.5  # noise initial parameter
+    max_epoch = 100  # maximum epoch during training
+    learning_rate = 1e-2  # initial learning rate
+    lr_decays = {i: 0.95 for i in range(0, 500)}  # decay rate of learning rate
+    # lr_decays = {i: 0.1 ** (1 / 100) for i in range(1, max_epoch)}
+
+    train_sum_dir = 'train_log'
+    saving = True
+    saving_path = None
+
+
+class STPLS3D:
+    def __init__(self, dataFolder, test_area_idx):
+        self.name = 'STPLS3D'
+        self.path = dataFolder
+        self.label_to_names = {0: 'ground',
+                               1: 'building',
+                               2: 'tree',
+                               3: 'car',
+                               4: 'lightPole',
+                               5: 'fence'}
+        self.num_classes = len(self.label_to_names)
+        self.label_values = np.sort([k for k, v in self.label_to_names.items()])
+        self.label_to_idx = {l: i for i, l in enumerate(self.label_values)}
+        self.ignored_labels = np.array([])
+
+        self.val_split = test_area_idx
+        self.all_files = glob.glob(os.path.join(self.path,'original_ply','*.ply'))
+
+        # Initiate containers
+        self.val_proj = []
+        self.val_labels = []
+        self.possibility = {}
+        self.min_possibility = {}
+        self.input_trees = {'training': [], 'validation': []}
+        self.input_colors = {'training': [], 'validation': []}
+        self.input_labels = {'training': [], 'validation': []}
+        self.input_names = {'training': [], 'validation': []}
+        self.load_sub_sampled_clouds(cfg.sub_grid_size)
+
+    def load_sub_sampled_clouds(self, sub_grid_size):
+        tree_path = os.path.join(self.path,'input_{:.3f}'.format(sub_grid_size))
+        for i, file_path in enumerate(self.all_files):
+            t0 = time.time()
+            cloud_name = os.path.basename(file_path)[:-4]
+            if self.val_split in cloud_name:
+                cloud_split = 'validation'
+            else:
+                cloud_split = 'training'
+
+            # Name of the input files
+            kd_tree_file = os.path.join(tree_path,'{:s}_KDTree.pkl'.format(cloud_name))
+            sub_ply_file = os.path.join(tree_path,'{:s}.ply'.format(cloud_name))
+
+            data = read_ply(sub_ply_file)
+            sub_colors = np.vstack((data['red'], data['green'], data['blue'])).T
+            sub_labels = data['class']
+
+            # Read pkl with search tree
+            with open(kd_tree_file, 'rb') as f:
+                search_tree = pickle.load(f)
+
+            self.input_trees[cloud_split] += [search_tree]
+            self.input_colors[cloud_split] += [sub_colors]
+            self.input_labels[cloud_split] += [sub_labels]
+            self.input_names[cloud_split] += [cloud_name]
+
+            size = sub_colors.shape[0] * 4 * 7
+            print('{:s} {:.1f} MB loaded in {:.1f}s'.format(os.path.basename(kd_tree_file), size * 1e-6, time.time() - t0))
+
+        print('\nPreparing reprojected indices for testing')
+
+        # Get validation and test reprojected indices
+        for i, file_path in enumerate(self.all_files):
+            t0 = time.time()
+            cloud_name = os.path.basename(file_path)[:-4]
+
+            # Validation projection and labels
+            if self.val_split in cloud_name:
+                proj_file = os.path.join(tree_path,'{:s}_proj.pkl'.format(cloud_name))
+                with open(proj_file, 'rb') as f:
+                    proj_idx, labels = pickle.load(f)
+                self.val_proj += [proj_idx]
+                self.val_labels += [labels]
+                print('{:s} done in {:.1f}s'.format(cloud_name, time.time() - t0))
+
+    # Generate the input data flow
+    def get_batch_gen(self, split):
+        if split == 'training':
+            num_per_epoch = cfg.train_steps * cfg.batch_size
+        elif split == 'validation':
+            num_per_epoch = cfg.val_steps * cfg.val_batch_size
+
+        self.possibility[split] = []
+        self.min_possibility[split] = []
+        # Random initialize
+        for i, tree in enumerate(self.input_colors[split]):
+            self.possibility[split] += [np.random.rand(tree.data.shape[0]) * 1e-3]
+            self.min_possibility[split] += [float(np.min(self.possibility[split][-1]))]
+
+
+        def spatially_regular_gen():
+            # Generator loop
+
+            for i in range(num_per_epoch):
+                ### file = open(r'D:\syntheticVSreal_experiements\RandLA-Net\data\temp%d.txt'%i,'w')
+                # Choose the cloud with the lowest probability
+                cloud_idx = int(np.argmin(self.min_possibility[split]))
+
+                # choose the point with the minimum of possibility in the cloud as query point
+                point_ind = np.argmin(self.possibility[split][cloud_idx])
+
+                # Get all points within the cloud from tree structure
+                points = np.array(self.input_trees[split][cloud_idx].data, copy=False)
+
+                # Center point of input region
+                center_point = points[point_ind, :].reshape(1, -1)
+
+                # Add noise to the center point
+                noise = np.random.normal(scale=cfg.noise_init / 10, size=center_point.shape)
+                pick_point = center_point + noise.astype(center_point.dtype)
+
+                # Check if the number of points in the selected cloud is less than the predefined num_points
+                if len(points) < cfg.num_points:
+                    # Query all points within the cloud
+                    queried_idx = self.input_trees[split][cloud_idx].query(pick_point, k=len(points))[1][0]
+                else:
+                    # Query the predefined number of points
+                    queried_idx = self.input_trees[split][cloud_idx].query(pick_point, k=cfg.num_points)[1][0]
+
+                # Shuffle index
+                queried_idx = DP.shuffle_idx(queried_idx)
+                # Get corresponding points and colors based on the index
+                queried_pc_xyz = points[queried_idx]
+                queried_pc_xyz = queried_pc_xyz - pick_point
+                queried_pc_colors = self.input_colors[split][cloud_idx][queried_idx]
+                queried_pc_labels = self.input_labels[split][cloud_idx][queried_idx]
+
+                # Update the possibility of the selected points
+                dists = np.sum(np.square((points[queried_idx] - pick_point).astype(np.float32)), axis=1)
+                delta = np.square(1 - dists / np.max(dists))
+                self.possibility[split][cloud_idx][queried_idx] += delta
+                self.min_possibility[split][cloud_idx] = float(np.min(self.possibility[split][cloud_idx]))
+
+                # up_sampled with replacement
+                if len(points) < cfg.num_points:
+                    queried_pc_xyz, queried_pc_colors, queried_idx, queried_pc_labels = \
+                        DP.data_aug(queried_pc_xyz, queried_pc_colors, queried_pc_labels, queried_idx, cfg.num_points)
+
+                # for i,point in enumerate(queried_pc_xyz):
+                #     file.write("%f,%f,%f,%f,%f,%f,%d\n"%(point[0],point[1],point[2],
+                #                                        queried_pc_colors[i][0],queried_pc_colors[i][1],queried_pc_colors[i][2],
+                #                                        queried_pc_labels[i]))
+                if True:
+                    yield (queried_pc_xyz.astype(np.float32),
+                           queried_pc_colors.astype(np.float32),
+                           queried_pc_labels,
+                           queried_idx.astype(np.int32),
+                           np.array([cloud_idx], dtype=np.int32))
+                # file.close()
+        gen_func = spatially_regular_gen
+        gen_types = (tf.float32, tf.float32, tf.int32, tf.int32, tf.int32)
+        gen_shapes = ([None, 3], [None, 3], [None], [None], [None])
+        return gen_func, gen_types, gen_shapes
+
+    @staticmethod
+    def get_tf_mapping2():
+        # Collect flat inputs
+        def tf_map(batch_xyz, batch_features, batch_labels, batch_pc_idx, batch_cloud_idx):
+            batch_features = tf.concat([batch_xyz, batch_features], axis=-1)
+            input_points = []
+            input_neighbors = []
+            input_pools = []
+            input_up_samples = []
+
+            for i in range(cfg.num_layers):
+                neighbour_idx = tf.py_func(DP.knn_search, [batch_xyz, batch_xyz, cfg.k_n], tf.int32)
+                sub_points = batch_xyz[:, :tf.shape(batch_xyz)[1] // cfg.sub_sampling_ratio[i], :]
+                pool_i = neighbour_idx[:, :tf.shape(batch_xyz)[1] // cfg.sub_sampling_ratio[i], :]
+                up_i = tf.py_func(DP.knn_search, [sub_points, batch_xyz, 1], tf.int32)
+                input_points.append(batch_xyz)
+                input_neighbors.append(neighbour_idx)
+                input_pools.append(pool_i)
+                input_up_samples.append(up_i)
+                batch_xyz = sub_points
+
+            input_list = input_points + input_neighbors + input_pools + input_up_samples
+            input_list += [batch_features, batch_labels, batch_pc_idx, batch_cloud_idx]
+
+            return input_list
+
+        return tf_map
+
+    def init_input_pipeline(self):
+        print('Initiating input pipelines')
+        cfg.ignored_label_inds = [self.label_to_idx[ign_label] for ign_label in self.ignored_labels]
+        gen_function, gen_types, gen_shapes = self.get_batch_gen('training')
+        gen_function_val, _, _ = self.get_batch_gen('validation')
+        self.train_data = tf.data.Dataset.from_generator(gen_function, gen_types, gen_shapes)
+        self.val_data = tf.data.Dataset.from_generator(gen_function_val, gen_types, gen_shapes)
+
+        self.batch_train_data = self.train_data.batch(cfg.batch_size)
+        self.batch_val_data = self.val_data.batch(cfg.val_batch_size)
+        map_func = self.get_tf_mapping2()
+
+        self.batch_train_data = self.batch_train_data.map(map_func=map_func)
+        self.batch_val_data = self.batch_val_data.map(map_func=map_func)
+
+        self.batch_train_data = self.batch_train_data.prefetch(cfg.batch_size)
+        self.batch_val_data = self.batch_val_data.prefetch(cfg.val_batch_size)
+
+        iter = tf.data.Iterator.from_structure(self.batch_train_data.output_types, self.batch_train_data.output_shapes)
+        self.flat_inputs = iter.get_next()
+        self.train_init_op = iter.make_initializer(self.batch_train_data)
+        self.val_init_op = iter.make_initializer(self.batch_val_data)
+
+    @staticmethod
+    def load_evaluation_points(file_path):
+        data = read_ply(file_path)
+        return np.vstack((data['x'], data['y'], data['z'])).T,\
+               np.vstack((data['red'], data['green'], data['blue'])).T
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--gpu', type=int, default=0, help='the number of GPUs to use [default: 0]')
+    parser.add_argument('--data_folder', type=str, default=r'E:\STPLS3D\STPLS3D_prepared\RealWorldData', help='The directory that contains all data')
+    parser.add_argument('--test_area', type=str, default='USC_GT', help='Which area to use for test, option: 1-6 [default: 5]')
+    parser.add_argument('--mode', type=str, default='train', help='options: train, test, vis')
+    parser.add_argument('--model_path', type=str, default='None', help='pretrained model path')
+    FLAGS = parser.parse_args()
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    # os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.gpu)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    Mode = FLAGS.mode
+
+    test_area = FLAGS.test_area
+    dataset = STPLS3D(FLAGS.data_folder,test_area)
+    dataset.init_input_pipeline()
+
+    if Mode == 'train':
+        model = Network(dataset, cfg)
+        model.train(dataset)
+    elif Mode == 'test':
+        cfg.saving = False
+        model = Network(dataset, cfg)
+        if os.path.isfile(FLAGS.model_path+'.meta'):
+            chosen_snap = FLAGS.model_path
+            tester = ModelTester(model, dataset, restore_snap=chosen_snap)
+            IoUs,m_IoU,acc = tester.test(model, dataset)
+
+        elif os.path.isdir(FLAGS.model_path):
+            checkpointList = [f for f in os.listdir(FLAGS.model_path) if f.endswith('.meta')]
+            IoUs_list = []
+            m_IoU_list = []
+            acc_list = []
+            for checkpoint in checkpointList:
+                tester = ModelTester(model, dataset, restore_snap=os.path.join(FLAGS.model_path,checkpoint.strip('.meta')))
+                IoUs,m_IoU,acc = tester.test(model, dataset)
+                IoUs_list.append(IoUs)
+                m_IoU_list.append(m_IoU)
+                acc_list.append(acc)
+
+            m_IoU_list = np.array(m_IoU_list)
+            best_mIOUindx = m_IoU_list.argmax()
+            print ("Best model: %s" % checkpointList[best_mIOUindx])
+            print ("Best mIOU: %s" % m_IoU_list[best_mIOUindx])
+            print ("Best IOUs: %s" % IoUs_list[best_mIOUindx])
+            print ("Best acc: %s" % acc_list[best_mIOUindx])
+        else:
+            chosen_snapshot = -1
+            logs = np.sort([os.path.join('results',f) for f in os.listdir('results') if f.startswith('Log')])
+            chosen_folder = logs[-1]
+            snap_path = chosen_folder +'/'+ 'snapshots'
+            snap_steps = [int(f[:-5].split('-')[-1]) for f in os.listdir(snap_path) if f[-5:] == '.meta']
+            chosen_step = np.sort(snap_steps)[-1]
+            chosen_snap = os.path.join(snap_path,'snap-{:d}'.format(chosen_step))
+            ### Model used
+            # chosen_snap = r'D:\syntheticVSreal_experiements\RandLA-Net\results\Log_2021-09-21_21-05-21\snapshots\snap-45501'
+
+            tester = ModelTester(model, dataset, restore_snap=chosen_snap)
+            IoUs,m_IoU,acc = tester.test(model, dataset)
---
+++ SCF-Net/helper_dp.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+import colorsys, random, os, sys
+from os.path import join
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'utils'))
+import cpp_wrappers.cpp_subsampling.grid_subsampling as cpp_subsampling
+import nearest_neighbors.lib.python.nearest_neighbors as nearest_neighbors
+
+
+class DataProcessing:
+    @staticmethod
+    def load_pc_semantic3d(filename):
+        pc_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.float16)
+        pc = pc_pd.values
+        return pc
+
+    @staticmethod
+    def load_label_semantic3d(filename):
+        label_pd = pd.read_csv(filename, header=None, delim_whitespace=True, dtype=np.uint8)
+        cloud_labels = label_pd.values
+        return cloud_labels
+
+    @staticmethod
+    def knn_search(support_pts, query_pts, k):
+        """
+        :param support_pts: points you have, B*N1*3
+        :param query_pts: points you want to know the neighbour index, B*N2*3
+        :param k: Number of neighbours in knn search
+        :return: neighbor_idx: neighboring points indexes, B*N2*k
+        """
+
+        neighbor_idx = nearest_neighbors.knn_batch(support_pts, query_pts, k, omp=True)
+        return neighbor_idx.astype(np.int32)
+
+    @staticmethod
+    def data_aug(xyz, color, labels, idx, num_out):
+        num_in = len(xyz)
+        dup = np.random.choice(num_in, num_out - num_in)
+        xyz_dup = xyz[dup, ...]
+        xyz_aug = np.concatenate([xyz, xyz_dup], 0)
+        color_dup = color[dup, ...]
+        color_aug = np.concatenate([color, color_dup], 0)
+        idx_dup = list(range(num_in)) + list(dup)
+        idx_aug = idx[idx_dup]
+        label_aug = labels[idx_dup]
+        return xyz_aug, color_aug, idx_aug, label_aug
+
+    @staticmethod
+    def shuffle_idx(x):
+        # random shuffle the index
+        idx = np.arange(len(x))
+        np.random.shuffle(idx)
+        return x[idx]
+
+
+    @staticmethod
+    def grid_sub_sampling(points, features=None, labels=None, grid_size=0.1, verbose=0):
+        """
+        CPP wrapper for a grid sub_sampling (method = barycenter for points and features
+        :param points: (N, 3) matrix of input points
+        :param features: optional (N, d) matrix of features (floating number)
+        :param labels: optional (N,) matrix of integer labels
+        :param grid_size: parameter defining the size of grid voxels
+        :param verbose: 1 to display
+        :return: sub_sampled points, with features and/or labels depending of the input
+        """
+
+        if (features is None) and (labels is None):
+            return cpp_subsampling.compute(points, sampleDl=grid_size, verbose=verbose)
+        elif labels is None:
+            return cpp_subsampling.compute(points, features=features, sampleDl=grid_size, verbose=verbose)
+        elif features is None:
+            return cpp_subsampling.compute(points, classes=labels, sampleDl=grid_size, verbose=verbose)
+        else:
+            return cpp_subsampling.compute(points, features=features, classes=labels, sampleDl=grid_size,
+                                           verbose=verbose)
+
+    @staticmethod
+    def IoU_from_confusions(confusions):
+        """
+        Computes IoU from confusion matrices.
+        :param confusions: ([..., n_c, n_c] np.int32). Can be any dimension, the confusion matrices should be described by
+        the last axes. n_c = number of classes
+        :return: ([..., n_c] np.float32) IoU score
+        """
+
+        # Compute TP, FP, FN. This assume that the second to last axis counts the truths (like the first axis of a
+        # confusion matrix), and that the last axis counts the predictions (like the second axis of a confusion matrix)
+        TP = np.diagonal(confusions, axis1=-2, axis2=-1)
+        TP_plus_FN = np.sum(confusions, axis=-1)
+        TP_plus_FP = np.sum(confusions, axis=-2)
+
+        # Compute IoU
+        IoU = TP / (TP_plus_FP + TP_plus_FN - TP + 1e-6)
+
+        # Compute mIoU with only the actual classes
+        mask = TP_plus_FN < 1e-3
+        counts = np.sum(1 - mask, axis=-1, keepdims=True)
+        mIoU = np.sum(IoU, axis=-1, keepdims=True) / (counts + 1e-6)
+
+        # If class is absent, place mIoU in place of 0 IoU to get the actual mean later
+        IoU += mask * mIoU
+        return IoU
+
+    @staticmethod
+    def get_class_weights(dataset_name):
+        # pre-calculate the number of points in each category
+        num_per_class = []
+        if dataset_name is 'STPLS3D':
+            num_per_class = np.array([5315782, 6445416, 3669377, 170255, 31557, 89423], dtype=np.int32)
+        weight = num_per_class / float(sum(num_per_class))
+        ce_label_weight = 1 / (weight + 0.02)
+        return np.expand_dims(ce_label_weight, axis=0)
